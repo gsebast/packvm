4699e0c78bd2bd87467cd7f3d1635ecba3e4a052
Sebastian Gerau <gsebast@web.de>
2019-06-24 21:09:05 +0200

    Updated Powershell script for input artifact cleanup.

--- _common/scripts/ps/helpers/cleanup_input_artifact.ps1
+++ _common/scripts/ps/helpers/cleanup_input_artifact.ps1
@@ -1,3 +1,3 @@
 # Delete the source folder after it is no longer needed
-echo "===> Deleting artifacts in source folder $env:SOURCE_FOLDER"
+echo "INFO: Deleting artifacts in source folder $env:SOURCE_FOLDER"
 Remove-Item -Recurse -Force $env:SOURCE_FOLDER
