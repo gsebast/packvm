408606cce866cdf4796a44011c751e62efaed608
Sebastian Gerau <gsebast@web.de>
2019-06-24 21:08:06 +0200

    Added Powershell script to copy OVF files.

---
+++ _common/scripts/ps/helpers/copy_ovf_file.ps1
@@ -0,0 +1,9 @@
+# Test if target folder exists and if not then create it
+if (!(Test-Path -Path $env:TARGET_FOLDER)) {
+  echo "INFO: Creating folder $env:TARGET_FOLDER"
+  New-Item -ItemType directory -Path $env:TARGET_FOLDER
+}
+
+# Copy all files matching the file pattern from the source to the target folder
+echo "INFO: Copying all files starting with $env:FILE_PATTERN from $env:SOURCE_FOLDER to $env:TARGET_FOLDER"
+Copy-Item -Path $env:SOURCE_FOLDER/$env:FILE_PATTERN -Destination $env:TARGET_FOLDER
