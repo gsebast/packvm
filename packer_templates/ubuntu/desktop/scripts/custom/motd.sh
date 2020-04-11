2e2dba5c0b1f71a55a9708165411d11024c5ff08
Sebastian Gerau <sg.infotech@protonmail.com>
2020-04-11 10:38:25 +0200

    Add custom motd files with Packvm message

---
+++ packer_templates/ubuntu/desktop/scripts/custom/motd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -eux
+
+packvm='
+This system is built by the Packvm project by gsebast
+More information can be found at https://github.com/gsebast/packvm
+It is based on the Boxcutter project
+More information can be found at https://github.com/boxcutter'
+
+if [ -d /etc/update-motd.d ]; then
+    MOTD_CONFIG='/etc/update-motd.d/99-packvm'
+
+    cat >> "$MOTD_CONFIG" <<PACKVM
+#!/bin/sh
+
+cat <<'EOF'
+$packvm
+EOF
+PACKVM
+
+    chmod 0755 "$MOTD_CONFIG"
+else
+    echo "$packvm" >> /etc/motd
+fi
