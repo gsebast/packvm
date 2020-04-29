31a164783f9e4b7473adbaa2acfe6ecda54a0e9b
Sebastian Gerau <sg.infotech@protonmail.com>
2020-04-29 17:39:45 +0200

    Customize motd.sh script to reflect packvm project

--- packer_templates/_common/scripts/motd.sh
+++ packer_templates/_common/scripts/motd.sh
@@ -1,21 +1,23 @@
 #!/bin/sh -eux
 
-bento='
-This system is built by the Bento project by Chef Software
+packvm='
+This system is built by the Packvm project by gsebast
+More information can be found at https://github.com/gsebast/packvm
+It is based on the Bento project by Chef Software
 More information can be found at https://github.com/chef/bento'
 
 if [ -d /etc/update-motd.d ]; then
-    MOTD_CONFIG='/etc/update-motd.d/99-bento'
+    MOTD_CONFIG='/etc/update-motd.d/99-packvm'
 
-    cat >> "$MOTD_CONFIG" <<BENTO
+    cat >> "$MOTD_CONFIG" <<PACKVM
 #!/bin/sh
 
 cat <<'EOF'
-$bento
+$packvm
 EOF
-BENTO
+PACKVM
 
     chmod 0755 "$MOTD_CONFIG"
 else
-    echo "$bento" >> /etc/motd
+    echo "$packvm" >> /etc/motd
 fi
